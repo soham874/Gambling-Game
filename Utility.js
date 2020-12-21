85cd8ab487a1cb5d728bdd0b8a99d754bf12c514
soham874 <soham874@gmail.com>
2020-12-21 19:59:25 +0530

    Added : Daily win or loss results for a month

--- Utility.js
+++ Utility.js
@@ -1,6 +1,12 @@
 var amount = 100
 var betAmount = 1
 
+var wonDay = []
+var wonAmount = []
+
+var lostDay = []
+var lostAmount = []
+
 class Utility {
 
     //play a turn of the game
@@ -20,17 +26,33 @@ class Utility {
         while(amount > 0.5*dayAmount && amount < 1.5*dayAmount)
             this.playGame()
 
+        return dayAmount
     }
 
     //month game simulation
     monthGame(){
-        for(let i = 1;i<=20;i++)
-            this.dayGame()
+        for(let i = 1;i<=20;i++){
+            var finalAmount = this.dayGame()
+
+            if(amount > finalAmount){
+                wonDay.push(i)
+                wonAmount.push(amount-finalAmount)
+            }else{
+                lostDay.push(i)
+                lostAmount.push(finalAmount-amount)
+            }
+        }
+            
 
         if(amount > 100)
             console.log("Amount won after a month is " + (amount - 100))
         else
-            console.log("Amount lost after a month is " + (100 - amount ))       
+            console.log("Amount lost after a month is " + (100 - amount ))   
+            
+        console.log("Days on which game was won : "+wonDay)
+        console.log("Amount won on corresponding days : "+wonAmount)
+        console.log("Days on which game was lost : "+lostDay)
+        console.log("Amount lost on corresponding days : "+lostAmount)
     }
 }
 
